157f2c46b19474e548f4da9b993ffaddf3111d27
edudscrc <eduardo.sa.coelho@gmail.com>
2025-03-17 00:56:27 -0300

    some css classes manually edited and colors from vscode theme

--- install.sh
+++ install.sh
@@ -3,5 +3,17 @@
 cd ./Adwaita
 ./parse-sass.sh
 mkdir -p ~/.themes/ArchEVA-01/gtk-3.0
+cat <<EOL >> "./gtk-contained-dark.css"
+
+box > scrolledwindow .view,
+box > scrolledwindow .view:backdrop {
+  background-color: #100c12;
+}
+
+box > scrolledwindow > treeview .view:selected:focus,
+box > scrolledwindow > treeview .view:selected { 
+  background-color: #ac7ec8; 
+}
+EOL
 cp ./gtk-contained-dark.css ~/.themes/ArchEVA-01/gtk-3.0/gtk.css
 cp -r ./assets ~/.themes/ArchEVA-01/gtk-3.0/assets
